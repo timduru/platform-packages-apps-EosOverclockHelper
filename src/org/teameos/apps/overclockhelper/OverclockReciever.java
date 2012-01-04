6307741994a90572f199450719bd6388cb783dd6
Chris Trotman <solarnz@teameos.org>
2012-01-04 21:31:30 +1300

    Clean up the code and add translation support.
    Change-Id: Ibdc73ada8fec787172c893ada025d4bacbb32e16

--- src/org/teameos/apps/overclockhelper/OverclockReciever.java
+++ src/org/teameos/apps/overclockhelper/OverclockReciever.java
@@ -4,13 +4,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-import android.R;
+import org.teameos.apps.overclockhelper.R;
+
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.util.Log;
 
 public class OverclockReciever extends BroadcastReceiver {
@@ -36,22 +38,18 @@ public class OverclockReciever extends BroadcastReceiver {
                     .getSystemService(Context.NOTIFICATION_SERVICE);
             long when = System.currentTimeMillis();
 
-            Notification notification = new Notification(R.drawable.ic_dialog_alert,
-                    "Overclocking Not Applied", when);
-            CharSequence contentTitle = "Eos Overclocking";
-            CharSequence contentText = "Values have not been applied due to recent reboot.";
-            // Intent notificationIntent = new Intent(this,
-            // OverclockReciever.class);
-
+            Resources resources = context.getResources();
+            Notification notification = new Notification(android.R.drawable.ic_dialog_alert,
+                    resources.getString(R.string.not_applied), when);
             Intent intent1 = new Intent("android.settings.eos.performance_settings");
             intent1.addCategory(Intent.CATEGORY_DEFAULT);
             intent1.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             PendingIntent contentIntent = PendingIntent.getActivity(context, 0, intent1, 0);
             notification.flags = Notification.FLAG_AUTO_CANCEL;
-            notification.setLatestEventInfo(context, contentTitle, contentText, contentIntent);
+            notification.setLatestEventInfo(context,
+                    resources.getString(R.string.notification_title),
+                    resources.getString(R.string.notification_text), contentIntent);
             mNotificationManager.notify(0, notification);
-
-            Log.i("eos", "there should be a notification now");
         }
     }
 
